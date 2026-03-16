46a4d34d3249cef06d919dba4ba8905ebe91f213
LoigiFan72 <stephenakins855@gmail.com>
2026-03-16 18:35:11 -0400

    Fix Things so that ARMCC doesnt hate me

--- include/nn/srv/svc_Api.h
+++ include/nn/srv/svc_Api.h
@@ -7,6 +7,5 @@ namespace nn {
 namespace srv {
     Result Initialize();
     Result GetServiceHandle(Handle* out, const char* service, s32, u32);
-    void ExitProcess(); //Project needs this
 } // Namespace srv
 } // Namespace nn
---
+++ include/nn/svc/svc_Api.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace nn{
+namespace svc{
+    void ExitProcess(){
+    };
+}
+};
